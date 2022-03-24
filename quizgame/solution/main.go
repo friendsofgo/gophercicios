f1d47ac80661ee82b2ceb85d67be5448c737ccac
aperezg <perezg.adrian@gmail.com>
2022-03-24 23:56:04 +0100

    segunda parte timer

--- quizgame/solution/main.go
+++ quizgame/solution/main.go
@@ -2,17 +2,23 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"encoding/csv"
 	"flag"
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
-const defaultFilename = "problems.csv"
+const (
+	defaultFilename = "problems.csv"
+	defaultTimeout  = 30 * time.Second
+)
 
 func main() {
 	filename := flag.String("file", defaultFilename, "fichero csv con las preguntas y las respuestas")
+	timeout := flag.Duration("timeout", defaultTimeout, "tiempo máximo para responder a las preguntas")
 	flag.Parse()
 
 	csvFile, err := os.Open(*filename)
@@ -27,19 +33,38 @@ func main() {
 		panic(err)
 	}
 
+	inReader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("¿Estás listo? Dale a enter cuándo quieras empezar")
+	_, _ = inReader.ReadString('\n')
+
+	ctx, cancel := context.WithTimeout(context.Background(), *timeout)
+	defer cancel()
+
 	var asserts int
+
 	for _, row := range data {
 		if len(row) != 2 {
 			panic("liada")
 		}
 		fmt.Println(row[0])
-		inReader := bufio.NewReader(os.Stdin)
-		in, _ := inReader.ReadString('\n')
 
-		if row[1] == strings.Replace(in, "\n", "", -1) {
-			asserts++
+		ch := make(chan string)
+
+		go func() {
+			in, _ := inReader.ReadString('\n')
+			ch <- in
+		}()
+
+		select {
+		case userInput := <-ch:
+			if row[1] == strings.Replace(userInput, "\n", "", -1) {
+				asserts++
+			}
+		case <-ctx.Done():
+			goto end
 		}
-
 	}
+end:
 	fmt.Printf("Has acertado: %d de %d\n", asserts, len(data))
 }
